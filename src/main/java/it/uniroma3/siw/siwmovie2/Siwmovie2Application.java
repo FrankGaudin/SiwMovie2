2cf8a8a04c42eaea2586fe9a3a2bad944315e612
Francesco Gaudino <72342418+FrankGaudin@users.noreply.github.com>
2023-06-23 12:15:00 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/siwmovie2/Siwmovie2Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwmovie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Siwmovie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Siwmovie2Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siwmovie2/Siwmovie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwmovie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Siwmovie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
